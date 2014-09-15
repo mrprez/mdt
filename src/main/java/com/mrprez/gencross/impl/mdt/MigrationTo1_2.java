fc056d588b80e2c54b348fdfb816b70d10e7125a
MrPrez <gagneret@yahoo.fr>
2014-09-16 01:45:11 +0200

    Correct migration

---
+++ src/main/java/com/mrprez/gencross/impl/mdt/MigrationTo1_2.java
@@ -0,0 +1,18 @@
+package com.mrprez.gencross.impl.mdt;
+
+import com.mrprez.gencross.Version;
+import com.mrprez.gencross.migration.MigrationPersonnage;
+import com.mrprez.gencross.migration.Migrator;
+
+public class MigrationTo1_2 implements Migrator {
+
+	@Override
+	public MigrationPersonnage migrate(MigrationPersonnage migrationPersonnage) throws Exception {
+		migrationPersonnage.getPluginDescriptor().setVersion(new Version(1, 2));
+		
+		return migrationPersonnage;
+	}
+	
+	
+
+}
